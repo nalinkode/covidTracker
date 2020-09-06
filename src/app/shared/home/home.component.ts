d8bf6a3fdbcb4cab9e98143cd208ece09f5c0a8f
nalinkode <nalinkode35@gmail.com>
2020-09-06 13:00:10 +0530

    1 file modified

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CovidServiceService } from '../service/covid-service.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private covidService :  CovidServiceService) { }
 
   ngOnInit() {
   }
