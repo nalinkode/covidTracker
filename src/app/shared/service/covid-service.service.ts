e2ab0ccabcc2dd4633536538a4e8978023f61056
nalinkode <nalinkode35@gmail.com>
2020-09-06 12:57:01 +0530

    1 file modified

--- src/app/shared/service/covid-service.service.ts
+++ src/app/shared/service/covid-service.service.ts
@@ -1,10 +1,24 @@
+  
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CovidServiceService {
 
-  constructor() { }
+ private globalData = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-05-2020.csv'
+
+ constructor(private http: HttpClient) { }
+
+ getGlobalData(){
+   this.http.get(this.globalData, ).pipe(
+     map(result => {
+       
+     })
+   )
+ }
+
 
 }
