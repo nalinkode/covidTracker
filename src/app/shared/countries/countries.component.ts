35be2ebeb55931cf5530cc441970fdaf3c3d7f4d
nalinkode <nalinkode35@gmail.com>
2020-09-06 15:11:33 +0530

    1 file modified

--- src/app/shared/countries/countries.component.ts
+++ src/app/shared/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CovidServiceService } from '../service/covid-service.service';
+import {GlobalDataSummary} from '../models/global-data-summary';
 
 @Component({
   selector: 'app-countries',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
-
-  constructor() { }
+ data : GlobalDataSummary[];
+ countries : string[] = [];
+constructor(private covidService : CovidServiceService ) { }
 
   ngOnInit() {
+      this.covidService.getGlobalData().subscribe(result=>{
+        this.data = result;
+        this.data.forEach(c=>{
+          this.countries.push(c.country);
+        })
+      })
   }
 
 }
