6503c934c15a18e6fcab49a5bdffcd6572bb1c8c
nalinkode <nalinkode35@gmail.com>
2020-09-06 12:32:32 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Router ,RouterModule, Routes } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { Error404Component } from './shared/error/error404/error404.component';
+import { Error500Component } from './shared/error/error500/error500.component';
+import { CountriesComponent } from './shared/countries/countries.component';
+import { HomeComponent } from './shared/home/home.component';
+import { routing } from './app.routing';
+import { DataServiceService } from './shared/service/data-service.service';
+
 
 import { AppComponent } from './app.component';
 
@@ -9,10 +20,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    routing,
+    HttpClientModule
 
   ],
   declarations: [
-    AppComponent,
+    AppComponent, 
+    NavBarComponent, 
+    Error404Component,
+    Error500Component, 
+    CountriesComponent,
+    HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
