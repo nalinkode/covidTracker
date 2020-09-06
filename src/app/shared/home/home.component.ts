cab78959a776fb46130d1175578745cc530e99e0
nalinkode <nalinkode35@gmail.com>
2020-09-06 16:03:48 +0530

    1 file modified

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -24,7 +24,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getGlobaleData();
-    this.initChart();
+   
   }
 
   initChart(){
@@ -61,6 +61,7 @@ export class HomeComponent implements OnInit {
         })
       }
     })
+     this.initChart();
   }
 
 }
