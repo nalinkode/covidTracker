12d13f4f81f6f4919fcdf8244d72895bfe0e9d0a
nalinkode <nalinkode35@gmail.com>
2020-09-06 13:46:03 +0530

    1 file modified

--- src/app/shared/service/covid-service.service.ts
+++ src/app/shared/service/covid-service.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import {GlobalDataSummary} from '../models/global-data-summary';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,18 @@ export class CovidServiceService {
  getGlobalData(){
    return this.http.get(this.globalData, {responseType: 'text'} ).pipe(
      map(result => {
+       let data : GlobalDataSummary[] = [];
        let rows  = result.split('\n');
        rows.forEach(row=>{
          let cols = row.split(/,(?=\S)/)
          console.log(cols)
+         data.push({
+           coountry : cols[3],
+           confirmed : cols[7],
+           deaths : cols[8],
+           recovered : cols[9],
+           active : cols[10]
+         })
        })
        return [];
 
